e525061a126172d11389ae7e0205b0bfb442325f
sialotobiko <sialo.tobiko@student.moringaschool.com>
2022-08-17 14:39:38 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-
 // Write your code here
+
+//multiplying num1 and num2
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+
+//generating a random number greater than 0
+let random = Math.floor(Math.random() * 10) + 1;
+
+//calculating the remainder of num3/num4
+let num3 = 10;
+let num4 = 6;
+let mod = num3 % num4;
+
+//finding the highest number in a set
+let max = Math.max(10, 5, 11, 20, 15);
